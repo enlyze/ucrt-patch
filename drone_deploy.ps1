3715136d90d8883efcf346cb3b6642075338903f
Colin Finck <c.finck@enlyze.com>
2021-10-21 16:23:19 +0200

    Auto-deploy the patched lib to the "ucrt-patched" repo on every commit.

---
+++ drone_deploy.ps1
@@ -0,0 +1,9 @@
+cmdkey /generic:git:https://github.com /user:$Env:GH_USERNAME /pass:$Env:GH_TOKEN
+git config --local user.name "drone-ci"
+git config --local user.email "ci@enlyze.com"
+
+git clone https://github.com/enlyze/ucrt-patched
+cd ucrt-patched
+copy ..\libucrt-patched.lib
+git commit -a -m "$Env:DRONE_COMMIT_MESSAGE"
+git push
